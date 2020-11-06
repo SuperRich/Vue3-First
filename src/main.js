cc57d33c0eb5cc700ad3cf1334e72be44db6a42e
SuperRich <r-hunter@live.co.uk>
2020-11-06 18:04:28 +0000

    init

---
+++ src/main.js
@@ -0,0 +1,12 @@
+﻿﻿const template = '<div>{{title}}</div>';
+const data = function data() {
+    return {
+        title: "Vue3"
+    };
+};
+
+
+const App = {data,template}
+
+
+Vue.createApp(App).mount("#hello");
